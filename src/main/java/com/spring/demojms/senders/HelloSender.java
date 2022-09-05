2219cf1c53087e6a3bf129b9ddb39e13501b2672
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 18:02:37 +0530

    add sendAndReceivedMessage()

--- src/main/java/com/spring/demojms/senders/HelloSender.java
+++ src/main/java/com/spring/demojms/senders/HelloSender.java
@@ -1,18 +1,26 @@
 package com.spring.demojms.senders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.demojms.config.JmsConfig;
 import com.spring.demojms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
 public class HelloSender {
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
+
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
 //        System.out.println("I'm sending a message.");
@@ -22,5 +30,27 @@ public class HelloSender {
                 .build();
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 //        System.out.println("Sent message.");
+    }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReceivedMessage(){
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello")
+                .build();
+        jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RECEIVED_QUEUE, new MessageCreator() {
+            @Override
+            public Message createMessage(Session session) throws JMSException {
+                Message helloMessage = null;
+                try {
+                    helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+                    helloMessage.setStringProperty("_type", "co.spring.demojms.model.HelloWorldMessage");
+                    System.out.println("Sending Hello");
+                    return helloMessage;
+                } catch (JsonProcessingException e) {
+                    throw new JMSException(e.getMessage());
+                }
+            }
+        });
     }
 }
