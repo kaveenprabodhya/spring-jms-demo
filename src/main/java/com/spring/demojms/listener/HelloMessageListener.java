56c9ef03a93cb20fda35ff20a94a98c051dc9543
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 17:19:53 +0530

    impl listener

--- src/main/java/com/spring/demojms/listener/HelloMessageListener.java
+++ src/main/java/com/spring/demojms/listener/HelloMessageListener.java
@@ -1,4 +1,22 @@
 package com.spring.demojms.listener;
 
+import com.spring.demojms.config.JmsConfig;
+import com.spring.demojms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+@Component
 public class HelloMessageListener {
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message){
+        System.out.println("I got a message");
+        System.out.println(helloWorldMessage);
+    }
 }
