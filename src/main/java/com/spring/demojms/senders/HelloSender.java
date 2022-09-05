b4d195d37dcaf657fe78085fbee49254087a4907
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 16:38:14 +0530

    add send Message

--- src/main/java/com/spring/demojms/senders/HelloSender.java
+++ src/main/java/com/spring/demojms/senders/HelloSender.java
@@ -1,4 +1,12 @@
 package com.spring.demojms.senders;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloSender {
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage(){
+        System.out.println("I'm sending a message.");
+    }
 }
