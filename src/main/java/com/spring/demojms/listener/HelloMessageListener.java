b385823062d95d183ef85c8230b1a2c7cc23e1c9
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 18:06:19 +0530

    add listener for Hello

--- src/main/java/com/spring/demojms/listener/HelloMessageListener.java
+++ src/main/java/com/spring/demojms/listener/HelloMessageListener.java
@@ -2,21 +2,40 @@ package com.spring.demojms.listener;
 
 import com.spring.demojms.config.JmsConfig;
 import com.spring.demojms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+    private final JmsTemplate jmsTemplate;
+
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage,
                        @Headers MessageHeaders headers,
                        Message message){
 //        System.out.println("I got a message");
 //        System.out.println(helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVED_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message) throws JMSException {
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("World!!")
+                .build();
+        jmsTemplate.convertAndSend((Destination) message.getJMSReplyTo(), payloadMsg);
     }
 }
