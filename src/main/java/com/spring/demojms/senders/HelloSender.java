ff9f9c33197fec6af858afe501265cc1499d4533
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 16:48:12 +0530

    implementing send Message to queue

--- src/main/java/com/spring/demojms/senders/HelloSender.java
+++ src/main/java/com/spring/demojms/senders/HelloSender.java
@@ -1,12 +1,26 @@
 package com.spring.demojms.senders;
 
+import com.spring.demojms.config.JmsConfig;
+import com.spring.demojms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Component
 public class HelloSender {
+    private final JmsTemplate jmsTemplate;
     @Scheduled(fixedRate = 2000)
     public void sendMessage(){
         System.out.println("I'm sending a message.");
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World")
+                .build();
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+        System.out.println("Sent message.");
     }
 }
