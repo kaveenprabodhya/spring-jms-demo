52ea9250f6a6998caf0e3235e547b6e10be81aac
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 13:51:10 +0530

    add props to HelloWorldMessage class

--- src/main/java/com/spring/demojms/model/HelloWorldMessage.java
+++ src/main/java/com/spring/demojms/model/HelloWorldMessage.java
@@ -1,4 +1,21 @@
 package com.spring.demojms.model;
 
-public class HelloWorldMessage {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    private final long serialVersionUId = 42L;
+
+    private UUID id;
+    private String message;
 }
