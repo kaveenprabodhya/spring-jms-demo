fa27f32bef4a12c5542173bc63d3aa4668b09f17
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-09-05 15:35:16 +0530

    config task executor

--- src/main/java/com/spring/demojms/config/TakConfig.java
+++ src/main/java/com/spring/demojms/config/TakConfig.java
@@ -1,4 +1,18 @@
 package com.spring.demojms.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+@EnableAsync
 public class TakConfig {
+    @Bean
+    TaskExecutor taskExecutor(){
+        return new SimpleAsyncTaskExecutor();
+    }
 }
